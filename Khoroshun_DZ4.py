272871df7c434d1e92e92251e3a7b0844f15905d
AndreyKhoroshiy <71352821+AndreyKhoroshiy@users.noreply.github.com>
2020-09-28 22:23:07 +0300

    HomeWork (lesson 4)

--- Khoroshun_DZ4.py
+++ Khoroshun_DZ4.py
@@ -3,3 +3,10 @@ for value in my_list:
     if value > 100:
         print(value)
 ##########################################
+my_list = [10, 25, 50, 110, 135, 145, 98]
+my_results = []
+for value in my_list:
+    if value > 100:
+        my_results.append(value)
+print(my_results)
+##########################################
