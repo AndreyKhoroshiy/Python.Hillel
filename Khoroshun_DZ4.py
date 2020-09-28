b08fa90c5e6e4521314b844e5d8e027935bcb93c
AndreyKhoroshiy <71352821+AndreyKhoroshiy@users.noreply.github.com>
2020-09-28 22:39:10 +0300

    HomeWork (lesson 4)

--- Khoroshun_DZ4.py
+++ Khoroshun_DZ4.py
@@ -10,3 +10,10 @@ for value in my_list:
         my_results.append(value)
 print(my_results)
 ##########################################
+my_list = [10, 25, 50, 110, 135, 145, 98]
+if len(my_list) < 2:
+    my_list.append(0)
+else:
+    my_list.append(my_list[-1] + my_list[-2])
+print(my_list)
+##########################################
