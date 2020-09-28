347aa2c61dd882794d528b1b8428a52302ce5b69
AndreyKhoroshiy <71352821+AndreyKhoroshiy@users.noreply.github.com>
2020-09-29 01:00:01 +0300

    HomeWork (lesson 4)

--- Khoroshun_DZ4.py
+++ Khoroshun_DZ4.py
@@ -7,14 +7,14 @@ my_list = [10, 25, 50, 110, 135, 145, 98]
 my_results = []
 for value in my_list:
     if value > 100:
-        my_results.append(value)
+        my_results.append(int(value))
 print(my_results)
 ##########################################
 my_list = [10, 25, 50, 110, 135, 145, 98]
 if len(my_list) < 2:
     my_list.append(0)
 else:
-    my_list.append(my_list[-1] + my_list[-2])
+    my_list.append(int(my_list[-1] + my_list[-2]))
 print(my_list)
 ##########################################
 value = input("Введите число")
