3543fe145c39813d5399047a2d8a7b2fbb942011
AndreyKhoroshiy <71352821+AndreyKhoroshiy@users.noreply.github.com>
2020-10-14 00:24:33 +0300

    HomeWork (lesson 7)

--- Khoroshun_DZ7.py
+++ Khoroshun_DZ7.py
@@ -2,3 +2,9 @@ from random import randint
 my_list = [randint(1, 100) for i in range(20)]
 print(my_list)
 ############################################################
+import random
+triangle = dict(A=(random.randrange(200), random.randrange(200)),
+                B=(random.randrange(200), random.randrange(200)),
+                C=(random.randrange(200), random.randrange(200)))
+print(triangle)
+#############################################################
