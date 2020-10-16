0f50aa46dd649add516aae484b7f1b506d816c77
AndreyKhoroshiy <71352821+AndreyKhoroshiy@users.noreply.github.com>
2020-10-17 00:19:34 +0300

    HomeWork (lesson 8)

---
+++ Khoroshun_DZ8.py
@@ -0,0 +1,21 @@
+def domains():
+    my_list_domains = []
+    for i in open(r"C:\Users\Тарас\Python.Hillel\Homeworks\domains.txt", 'r'):
+        my_list_domains.append(i[1:-1])
+    return my_list_domains
+
+
+print(domains())
+##################################################################################
+def names():
+    my_list_names = []
+    new_list = []
+    for i in open(r"C:\Users\Тарас\Python.Hillel\Homeworks\names.txt", 'r'):
+        my_list_names.append(i[:-1])
+    for index in my_list_names:
+        new_list.append(index.split()[1])
+    return new_list
+
+
+print(names())
+###################################################################################
