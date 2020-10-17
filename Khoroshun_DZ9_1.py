f1273b6a8f156075d9242c9a9e949dc437626225
AndreyKhoroshiy <71352821+AndreyKhoroshiy@users.noreply.github.com>
2020-10-18 01:32:23 +0300

    HomeWork (lesson 9_1)

---
+++ Khoroshun_DZ9_1.py
@@ -0,0 +1,16 @@
+import os
+
+
+def get_way(path="./"):
+    list_dir = os.listdir(path)
+    dict_ff = {"files": [], "folders": []}
+    for object in list_dir:
+        way_object = os.path.join(path, object)
+        if os.path.isfile(way_object):
+            dict_ff["files"].append(object)
+        else:
+            dict_ff["folders"].append(object)
+    print(dict_ff)
+
+
+get_way()
