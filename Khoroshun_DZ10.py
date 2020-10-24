5b3ec575e0319f45f2a962232a2b6389357618e1
AndreyKhoroshiy <71352821+AndreyKhoroshiy@users.noreply.github.com>
2020-10-24 14:48:47 +0300

    HomeWork (lesson 10)

--- Khoroshun_DZ10.py
+++ Khoroshun_DZ10.py
@@ -10,7 +10,9 @@ def generate_string():
         [' ', '.', ',', ':', ';', '-', '""', '/', '?', '!', '_', ' ']) + string.ascii_letters) for _ in range(91)))
     for i in range(9):
         my_str1 = (''.join(random.choice(string.ascii_letters + string.digits + random.choice([' ', '.', ',', ':', ';',
-                    '-', '""', '/' '?', '!', '_', ' '])) for _ in range(100)))
+                                                                                               '-', '""', '/' '?', '!',
+                                                                                               '_', ' '])) for _ in
+                           range(100)))
         my_list.append(my_str1 + '\n')
     my_list.append(my_str)
     new_str = ''.join(my_list)
@@ -19,18 +21,13 @@ def generate_string():
     return new_str
 
 
-final_string = generate_string()
-
-
 def my_dict():
-    list_key = [(''.join(random.choice(string.ascii_lowercase) for _ in range(5))) for _ in range(random.randrange(5, 21))]
+    list_key = [(''.join(random.choice(string.ascii_lowercase) for _ in range(5))) for _ in
+                range(random.randrange(5, 21))]
     list_value = random.choices([random.randrange(-100, 101), random.uniform(0, 1), True, False], k=len(list_key))
     my_new_dict = dict((key, value) for key, value in zip(list_key, list_value))
     print(my_new_dict)
     return my_new_dict
-
-
-final_dict = my_dict()
 
 
 def table():
@@ -41,48 +38,32 @@ def table():
     return my_table
 
 
-final_table = table()
-
-
 def write_txt(data, filename_with_path='data.txt'):
     with open(filename_with_path, 'w') as txt_file:
         txt_file.write(data)
-    return txt_file
-
-
-write_txt(final_string)
 
 
 def write_json(data, filename_with_path='data.json'):
     with open(filename_with_path, 'w') as json_file:
-        json_writer = json.dump(data, json_file)
-    return json_writer
-
-
-write_json(final_dict)
+        json.dump(data, json_file)
 
 
 def write_csv(data, filename_with_path='data.csv'):
     with open(filename_with_path, 'w') as csv_file:
         csv_writer = csv.writer(csv_file, delimiter=";", lineterminator='\n')
         csv_writer.writerows(data)
-    return csv_writer
-
-
-write_csv(final_table)
 
 
 def file_writer(filename_with_path):
     mode = filename_with_path.rsplit('.')[-1]
     if mode == 'txt':
-        data = write_txt(final_string, filename_with_path)
+        write_txt(generate_string(), filename_with_path)
     elif mode == 'json':
-        data = write_json(final_dict, filename_with_path)
+        write_json(my_dict(), filename_with_path)
     elif mode == 'csv':
-        data = write_csv(final_table, filename_with_path)
+        write_csv(table(), filename_with_path)
     else:
         raise Exception('Unsupported file format!')
-    return data
 
 
 file_writer('.txt')
