565508f35255b08261d8302a1e226c1343ae23b1
AndreyKhoroshiy <71352821+AndreyKhoroshiy@users.noreply.github.com>
2020-10-29 22:24:46 +0200

    HomeWork (lesson 11)

--- Pythone_DZ11/Khoroshun_DZ11.py
+++ Pythone_DZ11/Khoroshun_DZ11.py
@@ -1,5 +1,4 @@
 import json
-from operator import itemgetter
 
 
 def reading_file():
@@ -9,23 +8,14 @@ def reading_file():
     return data
 
 
+def sort_name(mp_dict):
+    name = mp_dict.get('name')
+    return name.split()[-1]
+
+
 read_data = reading_file()
-reading_file()
 
 
-def sort_name():
-    # data_name = sorted(read_data, key=itemgetter('name'))
-    for index in read_data:
-        elements = index.get('name')
-        my_string = "".join(elements)
-        name = my_string.split(" ")[-1]
-        # print(name)
-    return name
-
-
-data_sort_name = sort_name()
-sort_name()
-
-
-sorted_name_list = sorted(read_data, key=sort_name())
+sorted_name_list = sorted(read_data, key=sort_name)
 print(sorted_name_list)
+
