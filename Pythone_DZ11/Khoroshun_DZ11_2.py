2d2fc1bae2e21a0a3eeb1a13f20ede9aa77ffcfc
AndreyKhoroshiy <71352821+AndreyKhoroshiy@users.noreply.github.com>
2020-10-31 12:53:50 +0200

    HomeWork (lesson 11)

--- Pythone_DZ11/Khoroshun_DZ11_2.py
+++ Pythone_DZ11/Khoroshun_DZ11_2.py
@@ -2,17 +2,14 @@ import requests
 import json
 import csv
 
-res = requests.get('https://api.forismatic.com/api/1.0/')
-print(res.status_code)
-
 
 url = "https://api.forismatic.com/api/1.0/"
 
 
 def response(url):
-    get_res = requests.get(url, parameters=dict(method='getQuote', lang='ru', format='json'))
+    get_res = requests.get(url, params=dict(method='getQuote', lang='ru', format='json'))
     return get_res
 
 
-get_response = response()
-print(get_response)
+get_response = response(url)
+print(get_response.json())
