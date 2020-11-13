0bc65c32cb84da0d1d8cef04829bab8d363a8681
AndreyKhoroshiy <71352821+AndreyKhoroshiy@users.noreply.github.com>
2020-11-14 00:38:09 +0200

    HomeWork (lesson 13)

--- Khoroshun_DZ13.py
+++ Khoroshun_DZ13.py
@@ -17,10 +17,36 @@ def create_random_triangle():
     return tuple(coordinat)
 
 
-coordinats = create_random_triangle()
 create_random_triangle()
 
 
-def create_right_triangle(vert:tuple):
+def create_right_triangle(vert: tuple, square=100):
+    x1 = vert[0]
+    y1 = vert[1]
+    x2 = x1
+    y2 = y1 + ((2 * square) ** 0.5)
+    x3 = y2
+    y3 = y1
+    coordinat_triangle = [(x1, y1), (x2, y2), (x3, y3)]
+    print(coordinat_triangle)
+    return tuple(coordinat_triangle)
 
 
+my_coordinats = create_right_triangle((3, 5))
+
+
+def triangle_square():
+    print("Введите координаты вершин треугольника:")
+    x1 = float(input("x1 = "))
+    y1 = float(input("y1 = "))
+    x2 = float(input("x2 = "))
+    y2 = float(input("y2 = "))
+    x3 = float(input("y3 = "))
+    y3 = float(input("y3 = "))
+    square = ((x1 - x3) * (y2 - y3) - (y1 - y3) * (x2 - x3)) / 2
+    print("Площадь треугольника равна:")
+    print(square)
+    return square
+
+
+triangle_square()
